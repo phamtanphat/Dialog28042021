811174e92db962b8e953ca4d80265f45691cfc5d
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-07 20:20:30 +0700

    giao dien cơ bản của alert dialog

--- app/src/main/java/com/example/dialog28042021/MainActivity.java
+++ app/src/main/java/com/example/dialog28042021/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
                 builder.setMessage("Bạn có muốn nâng cấp ứng dụng hay không?");
                 builder.setIcon(R.mipmap.ic_launcher);
 
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Bắt đầu cập nhật", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Không cập nhật", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Chưa xác định", Toast.LENGTH_SHORT).show();
+                    }
+                });
                 builder.show();
             }
         });
