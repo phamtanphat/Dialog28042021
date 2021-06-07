8643788187b8ed2f784fb64a84cffb8b4126220c
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-07 21:23:50 +0700

    ket thuc phan co ban

--- app/src/main/java/com/example/dialog28042021/MainActivity.java
+++ app/src/main/java/com/example/dialog28042021/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button mBtnAlertDialog;
     int mIndex = -1;
+    String mResult = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,31 +23,43 @@ public class MainActivity extends AppCompatActivity {
 
         mBtnAlertDialog = findViewById(R.id.buttonAlertDialog);
 
+        String ten = "Pham Tan Phat";
+
+        Log.d("BBB",ten.substring(5,ten.length()));
+
         mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mResult = "";
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Chọn món ăn chính");
 
                 builder.setIcon(R.mipmap.ic_launcher);
                 builder.setCancelable(false);
 
-                // Single choice
+                // Multiple choice
                 String[] foodNames = {"Mỳ ý", "Pizza", "Gà rán", "Thịt nướng"};
+                boolean[] arrChecked = {false , false , false ,false};
 
-                builder.setSingleChoiceItems(foodNames, -1, new DialogInterface.OnClickListener() {
+                builder.setMultiChoiceItems(foodNames, arrChecked, new DialogInterface.OnMultiChoiceClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        mIndex = i;
+                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+
                     }
                 });
                 builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        if (mIndex == -1){
+                        for (int j = 0; j < arrChecked.length ; j++) {
+                            if (arrChecked[j]){
+                                mResult += foodNames[j] +  " ,";
+                            }
+                        }
+
+                        if (mResult.isEmpty()){
                             Toast.makeText(MainActivity.this, "Người dùng chưa chọn món", Toast.LENGTH_SHORT).show();
                         }else{
-                            Toast.makeText(MainActivity.this, "Bạn chọn " + foodNames[mIndex], Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, "Bạn đã chọn món " + mResult.substring(0,mResult.length() - 2), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
